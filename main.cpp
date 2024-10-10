7ad5436ad65c37f4a6a95df3cfee47ef762833c9
Hafiz Abdullah Yasir <hotstolen777@gmail.com>
2024-10-10 19:39:19 +0500

    first commit of signup

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 
 #include <iostream>
+#include <signup.cpp>
 using namespace std;
 
 int main(){
  cout<<"first commit in main"<<endl;
  cout<<"second commit in main"<<endl;
-
+ 
+signup();
 
     return 0;
 }
